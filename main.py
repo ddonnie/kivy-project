bb02657790bd17b449f0d2e6765f5d556b2e15d5
ddonnie <mohax91@gmail.com>
2016-07-02 17:04:53 +0300

    add interval is present remove interval is present carousel excess imports deleted

--- main.py
+++ main.py
@@ -4,33 +4,30 @@ Config.set('graphics','resizable',0)
 Config.set('graphics', 'width', '200')
 Config.set('graphics', 'height', '400')
 
-
 from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.gridlayout import GridLayout
 from kivy.app import App
-from kivy.properties import ObjectProperty, StringProperty
-from kivy.uix.modalview import ModalView
-from kivy.uix.scrollview import ScrollView
-from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.carousel import Carousel
 
 class Controller(AnchorLayout):
 
-
-
     def __init__(self, **kwargs):
         super(Controller, self).__init__(**kwargs)
         carousel = self.ids.minutes
-        for i in range(0,60):
-            carousel.add_widget(Label(text=str(i),font_name='Roboto',font_size=60))
+        for i in range(1,61):
+            carousel.add_widget(Label(text=str(i),font_size=60))
 
-    def set_min(self):
-        intervalcontainer = self.ids.intervalcontainer
-        intervalcontainer.add_widget(Label(text=str(self.ids.minutes.index)))
+    def set_interval(self):
+        self.ids.intervalcontainer.add_widget(Label(text=str(self.ids.minutes.index+1)+' min'))
         self.ids.increment.dismiss()
 
 
+    def remove_interval(self):
+        if not self.ids.intervalcontainer.children==[]:
+            for widget in self.ids.intervalcontainer.walk(restrict=True):
+                lastWidget = widget
+            self.ids.intervalcontainer.remove_widget(lastWidget)
+        else: pass
 
 class RunApp(App):
     def build(self):
